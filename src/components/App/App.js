613b73e109783fe395cfaa75a4b180ca21659619
Kat White <kat.white.a@gmail.com>
2021-08-04 12:20:34 -0600

    Write basic bones for App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,35 @@
-import './App.css';
+import React, { Component } from "react";
+import Characters from "./Characters";
+import "./App.css";
+import { fetchAllCharacters } from "../../apiCalls.js";
 
-function App() {
-  return (
-    <main className="App">
-      <header className="App-header">
-        <p>
-          Learn React
-        </p>
-      </header>
-    </main>
-  );
+class App extends Components {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+      error: "",
+    };
+  }
+
+  displayCharacters = () => {
+    fetchAllCharacters()
+      .then((data) => this.setState({ characters: data.docs }))
+      .catch(() =>
+        this.setState({
+          error: "Our apoloies, we are experiencing a server issue",
+        })
+      );
+  };
+
+  render() {
+    return (
+      <main>
+        <h1>Those of Middle Earth</h1>
+        <Characters characters={this.state.characters} />
+      </main>
+    );
+  }
 }
 
 export default App;
