247d99ab57e1b4c54a01166971f505e8ee5291a8
Kat White <kat.white.a@gmail.com>
2021-08-04 12:27:03 -0600

    Write function component for Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ".Card.css";
+
+const Card = ({ name, race, wikiUrl, gender, realm, birthday, key }) => {
+  return (
+    <div className="character-card" key={key}>
+      <h3>{name}</h3>
+      <p>{race}</p>
+      <p>{wikiUrl}</p>
+      <p>{gender}</p>
+      <p>{realm}</p>
+      <p>{birthday}</p>
+    </div>
+  );
+};
+
+export default Card;
