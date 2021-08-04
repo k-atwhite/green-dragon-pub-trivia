31df6e21904cdc838d7bb939cae7216f949907ce
Kat White <kat.white.a@gmail.com>
2021-08-04 11:30:08 -0600

    Update component file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      <header className="App-header">
+        <p>
+          Learn React
+        </p>
+      </header>
+    </main>
+  );
+}
+
+export default App;
