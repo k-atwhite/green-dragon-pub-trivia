9e206af1cac0b40cbe80f3c25456e54d285efae1
Kat White <kat.white.a@gmail.com>
2021-08-05 19:00:00 -0600

    Experiment with different ways of writing Quote component

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,8 +1,29 @@
-import React from ".react";
-import "/Quote.css";
+import React from "react";
+import "./Quote.css";
+import { getCharacterQuote } from "../../apiCalls";
 
-const Quote = ({ quote }) => {
-  return <h2>{quote}</h2>;
+const Quote = ({ characters }) => {
+  const randomNum = (num) => {
+    let randomNum = Math.floor(Math.random() * num + 1);
+    return randomNum;
+  };
+
+  const getRandomCharacter = (characters) => {
+    let randomCharacter = characters[this.randomNum(19)];
+    return randomCharacter.id;
+  };
+
+  const fetchQuotes = () => {
+    getCharacterQuote(getRandomCharacter()).then((data) =>
+      console.log(data.docs)
+    );
+  };
+
+  return (
+    <div>
+      <h2>A Quote!</h2>
+    </div>
+  );
 };
 
 export default Quote;
