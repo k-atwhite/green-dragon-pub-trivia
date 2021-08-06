5f37484cc7b3dc34a376e902ed64f061be2fa5ac
Kat White <kat.white.a@gmail.com>
2021-08-06 17:11:15 -0600

    Experiment with different ways to format the character API call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -11,17 +11,36 @@ export const getMainCharacters = () => {
   ).then((response) => response.json());
 };
 
-export const getCharacterQuote = (characterId) => {
-  return fetch(`https://the-one-api.dev/v2/character/${characterId}/quote`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
-    },
-  }).then((response) => response.json());
-};
+// export const getCharacterQuote = (characterId) => {
+//   return fetch(`https://the-one-api.dev/v2/character/${characterId}/quote`, {
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//       Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
+//     },
+//   }).then((response) => response.json());
+// };
 
-// How to improve performance
-// How can i get data to resolve at the same time?
-// How can the app work these asynchronously
-// Async await?
+// useEffect(() => {
+//   const headers = {
+//     Accept: "application/json",
+//     Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
+//   };
+//   const fetchData = async () => {
+//     const rawQuotes = await fetch("https://the-one-api.dev/v2/quote", {
+//       headers: headers,
+//     });
+//     const quotes = await rawQuotes.json();
+//     const quote = quotes.docs[Math.floor(Math.random() * quotes.docs.length)];
+//     setQuote(quote.dialog);
+//     const rawCharacters = await fetch(
+//       "https://the-one-api.dev/v2/character?_id=" + quote.character,
+//       { headers: headers }
+//     );
+//     const characters = await rawCharacters.json();
+//     const character = characters.docs[0];
+//     setCharacter(character.name);
+//   };
+
+//   fetchData();
+// }, []);
