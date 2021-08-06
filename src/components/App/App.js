306e2ae20d7366b514ba7cfecdfa560d0001995b
Kat White <kat.white.a@gmail.com>
2021-08-05 19:00:31 -0600

    Experiment with setting state for quotes array

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,22 @@ import React, { Component } from "react";
 import Characters from "../Characters/Characters";
 import Quote from "../Quote/Quote";
 import "./App.css";
-import { getMainCharacters } from "../../apiCalls.js";
-import { getQuote } from "../../apiCalls.js";
+import { getMainCharacters, getCharacterQuote } from "../../apiCalls.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       characters: [],
-      quote: "",
-      choices: [],
+      quotes: [],
       error: "",
     };
   }
+
+  randomNum = (num) => {
+    let randomNum = Math.floor(Math.random() * num + 1);
+    return randomNum;
+  };
 
   componentDidMount = () => {
     getMainCharacters()
@@ -24,18 +27,25 @@ class App extends Component {
           error: "Our apoloies, we are experiencing a server issue",
         })
       );
+    // getCharacterQuote().then((data) =>
+    //   this.setState({ quotes: data.docs })
+    // );
+    this.fetchRandomQuote();
   };
 
-  chooseRandomCharacter = () => {
-    let randomNum = Math.floor(Math.random() * 19);
+  fetchRandomQuote = () => {
+    let randomCharacter = this.state.characters[5];
+    getCharacterQuote(randomCharacter.id).then((data) =>
+      this.setState({ quotes: data.docs })
+    );
   };
 
   render() {
     return (
       <main>
         <h1>Who Said...?</h1>
-        {/* <Characters characters={this.state.characters} /> */}
-        <Quote quote={this.state.quote} />
+        <Characters characters={this.state.characters} />
+        {/* <Quote characters={this.state.characters} /> */}
       </main>
     );
   }
