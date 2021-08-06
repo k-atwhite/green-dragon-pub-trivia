e53e40ecad4914d3b3d584129fe81e777187ddde
Kat White <kat.white.a@gmail.com>
2021-08-06 17:11:51 -0600

    Simply show that state has all characters and one random character

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,94 +1,28 @@
-import React, { Component } from "react";
-import Characters from "../Characters/Characters";
-// import Quote from "../Quote/Quote";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { getMainCharacters, getCharacterQuote } from "../../apiCalls.js";
+import { getMainCharacters } from "../../apiCalls.js";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      characters: [],
-      characterIds: [
-        "5cd99d4bde30eff6ebccfbe6",
-        "5cd99d4bde30eff6ebccfc07",
-        "5cd99d4bde30eff6ebccfc38",
-        "5cd99d4bde30eff6ebccfc57",
-        "5cd99d4bde30eff6ebccfcc8",
-        "5cd99d4bde30eff6ebccfcef",
-        "5cd99d4bde30eff6ebccfc15",
-        "5cd99d4bde30eff6ebccfd06",
-        "5cd99d4bde30eff6ebccfea0",
-        "5cd99d4bde30eff6ebccfd23",
-        "5cd99d4bde30eff6ebccfe9e",
-        "5cd99d4bde30eff6ebccfd81",
-        "5cd99d4bde30eff6ebccfc7c",
-        "5cd99d4bde30eff6ebccfe2e",
-        "5cd99d4bde30eff6ebccfd0d",
-        "5cd99d4bde30eff6ebccfea4",
-        "5cd99d4bde30eff6ebccfe19",
-        "5cdbdecb6dc0baeae48cfa59",
-      ],
-      quotes: [],
-      error: "",
-    };
-  }
+const App = () => {
+  const [allCharacters, setAllCharacters] = useState([]);
+  // const [quote, setQuote] = useState("");
+  const [character, setCharacter] = useState();
 
-  // componentDidMount() {
-  //   getMainCharacters()
-  //     .then((data) => this.setState({ characters: data.docs }))
-  //     .then(() => this.setQuote());
-  // }
+  useEffect(() => {
+    getMainCharacters()
+      .then((data) => setAllCharacters(data.docs))
+      .then(() => {
+        let randomCharacter =
+          allCharacters[Math.floor(Math.random() * allCharacters.length)];
+        setCharacter(randomCharacter);
+      });
+  }, []);
 
-  // If i had state that already had ALL the IDs
-  componentDidMount() {
-    getMainCharacters().then((data) => {
-      this.setState({ characters: data.docs });
-      return this.state.characterIds.forEach((characterId) =>
-        getCharacterQuote(characterId).then((data) =>
-          this.setState({ quotes: [...this.state.quotes, data] })
-        )
-      );
-    });
-  }
-
-  // PUT THIS IN UTILS
-  randomNum = (num) => {
-    let randomNum = Math.floor(Math.random() * num + 1);
-    return randomNum;
-  };
-
-  // setQuote = () => {
-  //   if (this.state.characters.length) {
-  //     let randomCharacterId =
-  //       this.state.characters[this.randomNum(this.state.characters.length)][
-  //         "_id"
-  //       ];
-  //     getCharacterQuote(randomCharacterId).then((data) =>
-  //       // run cleaning helper function here
-  //       // whatever comes out of that cleaning function - use that variable to set state
-  //       this.setState({
-  //         quote: data.docs[this.randomNum(data.docs.length)]["dialog"],
-  //       })
-  //     );
-  //   }
-  // };
-
-  render() {
-    return (
-      <main>
-        <h1>Which hero of the Third Age spoke these words?</h1>
-        <Characters characters={this.state.characters} />
-        {/* <Quote quotes={this.state.quotes} /> */}
-      </main>
-    );
-  }
-}
+  return (
+    <main>
+      {/* <h1>{quote}</h1> */}
+      {/* <h2>- {character}</h2> */}
+    </main>
+  );
+};
 
 export default App;
-
-// .catch(() =>
-//         this.setState({
-//           error: "Our apologies, we are experiencing a server issue",
-//         })
-//       );
