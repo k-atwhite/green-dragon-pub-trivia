deb822348f3e214a5a3f310867c5f3da3f09346d
Kat White <kat.white.a@gmail.com>
2021-08-05 21:32:44 -0600

    Create function to choose random characters

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -3,17 +3,31 @@ import Card from "../Card/Card";
 import "./Characters.css";
 
 const Characters = ({ characters }) => {
-  const characterCards = characters.map((character) => {
+  const characterNames = characters.map((character) => {
+    return character.name;
+  });
+
+  // const randomOptions = random.sample(characterNames, 3);
+  let character1 =
+    characterNames[Math.floor(Math.random() * characterNames.length)];
+  let character2 =
+    characterNames[Math.floor(Math.random() * characterNames.length)];
+  let character3 =
+    characterNames[Math.floor(Math.random() * characterNames.length)];
+
+  const randomOptions = [character1, character2, character3];
+
+  const characterCards = randomOptions.map((character) => {
     return (
       <Card
         key={character._id}
         id={character._id}
         name={character.name}
-        race={character.race}
-        gender={character.gender}
-        realm={character.realm}
-        birthday={character.birth}
-        wikiUrl={character.wikiUrl}
+        // race={character.race}
+        // gender={character.gender}
+        // realm={character.realm}
+        // birthday={character.birth}
+        // wikiUrl={character.wikiUrl}
       ></Card>
     );
   });
