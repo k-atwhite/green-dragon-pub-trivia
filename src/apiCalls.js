c75cab15a0b5fd79c01f89099997fd7f98e8e590
Kat White <kat.white.a@gmail.com>
2021-08-06 17:55:07 -0600

    Continue experimenting with async calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-export const getMainCharacters = () => {
-  return fetch(
+export const getMainCharacters = async () => {
+  const mainCharacters = await fetch(
     "https://the-one-api.dev/v2/character?name=Gandalf,Frodo Baggins,Aragorn II Elessar,Legolas,Arwen,Galadriel,Gimli,Boromir,Bilbo Baggins,Samwise Gamgee,Gollum,Saruman,Peregrin Took,Meriadoc Brandybuck,Elrond,Théoden,Faramir,Éowyn",
     {
       method: "GET",
@@ -8,8 +8,42 @@ export const getMainCharacters = () => {
         Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
       },
     }
-  ).then((response) => response.json());
+  );
+  const characters = await mainCharacters.json();
+  const character =
+    characters.docs[Math.floor(Math.random() * characters.docs.length)];
+  return character;
 };
+
+export const getCharacterQuote = async (characterId) => {
+  const characterQuotes = await fetch(
+    `https://the-one-api.dev/v2/character/${characterId}/quote`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
+      },
+    }
+  );
+  const quotes = await characterQuotes.json();
+  const quote =
+    characterQuotes.docs[Math.floor(Math.random() * quotes.docs.length).dialog];
+  return quote;
+};
+
+// export const getMainCharacters = () => {
+//   return fetch(
+//     "https://the-one-api.dev/v2/character?name=Gandalf,Frodo Baggins,Aragorn II Elessar,Legolas,Arwen,Galadriel,Gimli,Boromir,Bilbo Baggins,Samwise Gamgee,Gollum,Saruman,Peregrin Took,Meriadoc Brandybuck,Elrond,Théoden,Faramir,Éowyn",
+//     {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//         Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
+//       },
+//     }
+//   ).then((response) => response.json());
+// };
 
 // export const getCharacterQuote = (characterId) => {
 //   return fetch(`https://the-one-api.dev/v2/character/${characterId}/quote`, {
@@ -20,27 +54,3 @@ export const getMainCharacters = () => {
 //     },
 //   }).then((response) => response.json());
 // };
-
-// useEffect(() => {
-//   const headers = {
-//     Accept: "application/json",
-//     Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
-//   };
-//   const fetchData = async () => {
-//     const rawQuotes = await fetch("https://the-one-api.dev/v2/quote", {
-//       headers: headers,
-//     });
-//     const quotes = await rawQuotes.json();
-//     const quote = quotes.docs[Math.floor(Math.random() * quotes.docs.length)];
-//     setQuote(quote.dialog);
-//     const rawCharacters = await fetch(
-//       "https://the-one-api.dev/v2/character?_id=" + quote.character,
-//       { headers: headers }
-//     );
-//     const characters = await rawCharacters.json();
-//     const character = characters.docs[0];
-//     setCharacter(character.name);
-//   };
-
-//   fetchData();
-// }, []);
