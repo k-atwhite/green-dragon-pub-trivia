903464499e3710f04cf949282ff14262a7b0c633
Kat White <kat.white.a@gmail.com>
2021-08-07 10:45:26 -0600

    Remove comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,36 +1,14 @@
 import { useEffect, useState } from "react";
-import { getMainCharacters, getCharacterQuotes } from "../../apiCalls.js";
-// import List from "../List/List";
+import { getMainCharacters } from "../../apiCalls.js";
 import Quote from "../Quote/Quote";
 
 import "./App.css";
 
 const App = () => {
   const [character, setCharacter] = useState([]);
-  // const [quotes, setQuotes] = useState([]);
-  // const [characterIds] = useState([
-  //   "5cd99d4bde30eff6ebccfbe6",
-  //   "5cd99d4bde30eff6ebccfc07",
-  //   "5cd99d4bde30eff6ebccfc38",
-  //   "5cd99d4bde30eff6ebccfc57",
-  //   "5cd99d4bde30eff6ebccfcc8",
-  //   "5cd99d4bde30eff6ebccfcef",
-  //   "5cd99d4bde30eff6ebccfc15",
-  //   "5cd99d4bde30eff6ebccfd06",
-  //   "5cd99d4bde30eff6ebccfea0",
-  //   "5cd99d4bde30eff6ebccfd23",
-  //   "5cd99d4bde30eff6ebccfe9e",
-  //   "5cd99d4bde30eff6ebccfd81",
-  //   "5cd99d4bde30eff6ebccfc7c",
-  //   "5cd99d4bde30eff6ebccfe2e",
-  //   "5cd99d4bde30eff6ebccfd0d",
-  //   "5cd99d4bde30eff6ebccfea4",
-  //   "5cd99d4bde30eff6ebccfe19",
-  //   "5cdbdecb6dc0baeae48cfa59",
-  // ]);
 
   useEffect(() => {
-    getMainCharacters().then((data) => setCharacter(data));
+    getMainCharacters().then((data) => setCharacter(data.docs[0]));
 
     // characterIds.forEach((characterId) =>
     //   getCharacterQuote(characterId).then((data) =>
@@ -41,9 +19,7 @@ const App = () => {
 
   return (
     <main>
-      {/* <h2>- {quote}</h2> */}
-      <h2>- {character.name}</h2>
-      {/* <List quotes={quotes} speaker={character.name} /> */}
+      <h2>Hero: {character.name}</h2>
       <Quote characterId={character._id} />
     </main>
   );
