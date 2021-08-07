108e8438bd9ae74e9972d8aaa6fa01d3677e0608
Kat White <kat.white.a@gmail.com>
2021-08-07 09:45:20 -0600

    Add loading and error handling

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -5,14 +5,31 @@ import { getCharacterQuotes } from "../../apiCalls.js";
 const Quote = ({ characterId }) => {
   const [quotes, setQuotes] = useState([]);
   const [randomQuote, setRandomQuote] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (characterId) {
-      getCharacterQuotes(characterId).then((data) => setQuotes(data.docs));
-    }
-    setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)].dialog);
+    console.log(characterId);
+
+    setLoading(true);
+
+    getCharacterQuotes(characterId)
+      .then((data) => setQuotes(data.docs))
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() =>
+        setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)].dialog)
+      )
+      .finally(() => setLoading(false));
   }, []);
 
+  if (loading) {
+    return (
+      <div className="quote-container">
+        <h2>Data is loading</h2>
+      </div>
+    );
+  }
   return (
     <div className="quote-container">
       <h2>{randomQuote}</h2>
