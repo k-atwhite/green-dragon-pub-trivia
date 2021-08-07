0619bf6458b3bf48978e05eadfaa04f0d805a7a9
Kat White <kat.white.a@gmail.com>
2021-08-07 09:07:50 -0600

    Remove need for CharacterID array

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,42 @@
 import { useEffect, useState } from "react";
-import { getMainCharacters, getCharacterQuote } from "../../apiCalls.js";
-import List from "../List/List";
+import { getMainCharacters, getCharacterQuotes } from "../../apiCalls.js";
+// import List from "../List/List";
+import Quote from "../Quote/Quote";
+
 import "./App.css";
 
 const App = () => {
   const [character, setCharacter] = useState([]);
-  const [quotes, setQuotes] = useState([]);
-  const [characterIds] = useState([
-    "5cd99d4bde30eff6ebccfbe6",
-    "5cd99d4bde30eff6ebccfc07",
-    "5cd99d4bde30eff6ebccfc38",
-    "5cd99d4bde30eff6ebccfc57",
-    "5cd99d4bde30eff6ebccfcc8",
-    "5cd99d4bde30eff6ebccfcef",
-    "5cd99d4bde30eff6ebccfc15",
-    "5cd99d4bde30eff6ebccfd06",
-    "5cd99d4bde30eff6ebccfea0",
-    "5cd99d4bde30eff6ebccfd23",
-    "5cd99d4bde30eff6ebccfe9e",
-    "5cd99d4bde30eff6ebccfd81",
-    "5cd99d4bde30eff6ebccfc7c",
-    "5cd99d4bde30eff6ebccfe2e",
-    "5cd99d4bde30eff6ebccfd0d",
-    "5cd99d4bde30eff6ebccfea4",
-    "5cd99d4bde30eff6ebccfe19",
-    "5cdbdecb6dc0baeae48cfa59",
-  ]);
+  // const [quotes, setQuotes] = useState([]);
+  // const [characterIds] = useState([
+  //   "5cd99d4bde30eff6ebccfbe6",
+  //   "5cd99d4bde30eff6ebccfc07",
+  //   "5cd99d4bde30eff6ebccfc38",
+  //   "5cd99d4bde30eff6ebccfc57",
+  //   "5cd99d4bde30eff6ebccfcc8",
+  //   "5cd99d4bde30eff6ebccfcef",
+  //   "5cd99d4bde30eff6ebccfc15",
+  //   "5cd99d4bde30eff6ebccfd06",
+  //   "5cd99d4bde30eff6ebccfea0",
+  //   "5cd99d4bde30eff6ebccfd23",
+  //   "5cd99d4bde30eff6ebccfe9e",
+  //   "5cd99d4bde30eff6ebccfd81",
+  //   "5cd99d4bde30eff6ebccfc7c",
+  //   "5cd99d4bde30eff6ebccfe2e",
+  //   "5cd99d4bde30eff6ebccfd0d",
+  //   "5cd99d4bde30eff6ebccfea4",
+  //   "5cd99d4bde30eff6ebccfe19",
+  //   "5cdbdecb6dc0baeae48cfa59",
+  // ]);
 
   useEffect(() => {
     getMainCharacters().then((data) => setCharacter(data));
 
-    characterIds.forEach((characterId) =>
-      getCharacterQuote(characterId).then((data) =>
-        setQuotes([...quotes, data.docs])
-      )
-    );
+    // characterIds.forEach((characterId) =>
+    //   getCharacterQuote(characterId).then((data) =>
+    //     setQuotes([...quotes, data.docs])
+    //   )
+    // );
   }, []);
 
   return (
@@ -42,6 +44,7 @@ const App = () => {
       {/* <h2>- {quote}</h2> */}
       <h2>- {character.name}</h2>
       {/* <List quotes={quotes} speaker={character.name} /> */}
+      <Quote characterId={character._id} />
     </main>
   );
 };
