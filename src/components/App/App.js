12e0dbed33a6159c7f50ceb111c73fab770b901d
Kat White <kat.white.a@gmail.com>
2021-08-07 16:57:04 -0600

    Remove comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  getMainCharacter,
-  getCharacterQuotes,
-  getAllCharacters,
-} from "../../apiCalls.js";
+import { getAllCharacters } from "../../apiCalls.js";
 import { Route, Switch, Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import Quote from "../Quote/Quote";
@@ -11,9 +7,6 @@ import "./App.css";
 
 const App = () => {
   const [allCharacters, setAllCharacters] = useState([]);
-  // const [quotes, setQuotes] = useState([]);
-  // const [randomQuote, setRandomQuote] = useState("");
-  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getAllCharacters().then((data) => setAllCharacters(data.docs));
@@ -60,41 +53,3 @@ const App = () => {
 };
 
 export default App;
-
-// import { useEffect, useState } from "react";
-// import { getMainCharacters } from "../../apiCalls.js";
-// import Quote from "../Quote/Quote";
-// import { Route, Switch, NavLink } from "react-router-dom";
-
-// import "./App.css";
-
-// const App = () => {
-//   const [character, setCharacter] = useState([]);
-
-//   useEffect(() => {
-//     getMainCharacters().then((data) => setCharacter(data.docs[0]));
-//   }, []);
-
-//   return (
-//     <main>
-//       <nav>
-//         <NavLink exact to="/" className="nav-link">
-//           Home
-//         </NavLink>
-//         <NavLink exact to="/trivia" className="nav-link">
-//           Quote Trivia
-//         </NavLink>
-//         <NavLink exact to="/heroes" className="nav-link">
-//           Heroes
-//         </NavLink>
-//         <NavLink exact to="/wise-words" className="nav-link">
-//           Wise Words
-//         </NavLink>
-//       </nav>
-//       <Route path="/" />
-//       <Route path="/trivia" render={() => <Quote character={character} />} />
-//     </main>
-//   );
-// };
-
-// export default App;
--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import "./Quote.css";
 import { getCharacterQuotes } from "../../apiCalls.js";
-import List from "../List/List";
 
 const Quote = ({ allCharacters }) => {
   const [character, setCharacter] = useState({});
@@ -50,48 +49,3 @@ const Quote = ({ allCharacters }) => {
 };
 
 export default Quote;
-
-// import { useEffect, useState } from "react";
-// import "./Quote.css";
-// import { getCharacterQuotes } from "../../apiCalls.js";
-// import List from "../List/List";
-
-// const Quote = ({ character }) => {
-//   const [quotes, setQuotes] = useState([]);
-//   const [randomQuote, setRandomQuote] = useState("");
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     setLoading(true);
-
-//     if (character) {
-//       getCharacterQuotes(character._id)
-//         .then((data) => setQuotes(data.docs))
-//         .catch((err) => {
-//           console.log(err);
-//         })
-//         .finally(() => setLoading(false));
-//     }
-//   }, [character]);
-
-//   useEffect(() => {
-//     if (quotes.length && !randomQuote) {
-//       setRandomQuote(quotes[0]["dialog"]);
-//     }
-//   }, [quotes]);
-
-//   return (
-//     <div className="quote-container">
-//       {loading && <h2>Data is loading</h2>}
-//       {quotes.length && (
-//         <div>
-//           <h2>{randomQuote}</h2>
-//           <h3>{character.name}</h3>
-//         </div>
-//       )}
-//       {/* <List quotes={quotes} key={characterId} /> */}
-//     </div>
-//   );
-// };
-
-// export default Quote;
