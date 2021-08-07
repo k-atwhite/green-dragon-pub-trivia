7ed58c6d2fa5b4edbc12f10fdb8471beb05bbe73
Kat White <kat.white.a@gmail.com>
2021-08-07 10:14:03 -0600

    Add character id array for potential refactor

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -17,20 +17,16 @@ export const getMainCharacters = async () => {
   return character;
 };
 
-export const getCharacterQuotes = async (characterId) => {
+export const getCharacterQuotes = (characterId) => {
   console.log("Quote API WAS CALLED");
 
-  const response = await fetch(
-    `https://the-one-api.dev/v2/character/${characterId}/quote`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
-      },
-    }
-  );
-  return await response.json();
+  return fetch(`https://the-one-api.dev/v2/character/${characterId}/quote`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
+    },
+  }).then((response) => response.json());
 };
 
 //  const names = [
@@ -53,39 +49,23 @@ export const getCharacterQuotes = async (characterId) => {
 //    "Éowyn",
 //  ];
 
-// export const getCharacterQuote = async (characterId) => {
-//   const characterQuotes = await fetch(
-//     `https://the-one-api.dev/v2/character/${characterId}/quote`,
-//     {
-//       method: "GET",
-//       headers: {
-//         Accept: "application/json",
-//         Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
-//       },
-//     }
-//   );
-//
-// };
-
-// export const getMainCharacters = () => {
-//   return fetch(
-//     "https://the-one-api.dev/v2/character?name=Gandalf,Frodo Baggins,Aragorn II Elessar,Legolas,Arwen,Galadriel,Gimli,Boromir,Bilbo Baggins,Samwise Gamgee,Gollum,Saruman,Peregrin Took,Meriadoc Brandybuck,Elrond,Théoden,Faramir,Éowyn",
-//     {
-//       method: "GET",
-//       headers: {
-//         Accept: "application/json",
-//         Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
-//       },
-//     }
-//   ).then((response) => response.json());
-// };
-
-// export const getCharacterQuote = (characterId) => {
-//   return fetch(`https://the-one-api.dev/v2/character/${characterId}/quote`, {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json",
-//       Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
-//     },
-//   }).then((response) => response.json());
-// };
+// let characterIds = [
+//   "5cd99d4bde30eff6ebccfbe6",
+//   "5cd99d4bde30eff6ebccfc07",
+//   "5cd99d4bde30eff6ebccfc38",
+//   "5cd99d4bde30eff6ebccfc57",
+//   "5cd99d4bde30eff6ebccfcc8",
+//   "5cd99d4bde30eff6ebccfcef",
+//   "5cd99d4bde30eff6ebccfc15",
+//   "5cd99d4bde30eff6ebccfd06",
+//   "5cd99d4bde30eff6ebccfea0",
+//   "5cd99d4bde30eff6ebccfd23",
+//   "5cd99d4bde30eff6ebccfe9e",
+//   "5cd99d4bde30eff6ebccfd81",
+//   "5cd99d4bde30eff6ebccfc7c",
+//   "5cd99d4bde30eff6ebccfe2e",
+//   "5cd99d4bde30eff6ebccfd0d",
+//   "5cd99d4bde30eff6ebccfea4",
+//   "5cd99d4bde30eff6ebccfe19",
+//   "5cdbdecb6dc0baeae48cfa59",
+// ]
