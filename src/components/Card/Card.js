469c907f7435ccfbafb3479ea7e3bcc183f228dd
Kat White <kat.white.a@gmail.com>
2021-08-07 17:17:35 -0600

    Render more character information

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ name }) => {
+const Card = ({ name, race, gender, realm, birthday }) => {
   return (
-    <div className="quote-card">
+    <div className="card">
       <h2>{name}</h2>
+      <p>{race}</p>
+      <p>{gender}</p>
+      <p>Realm: {realm}</p>
+      <p>Birth: {birthday}</p>
     </div>
   );
 };
