efd6a4b5031d4894e2ebde68bb2fbe26a885d56c
Kat White <kat.white.a@gmail.com>
2021-08-07 17:05:59 -0600

    Refactor list to show characters

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,11 +2,9 @@ import React from "react";
 import Card from "../Card/Card";
 import "./List.css";
 
-const List = ({ quotes, key }) => {
-  console.log(quotes, "quotes");
-  let quoteCards = quotes.map((quote) => {
-    console.log(quote);
-    return <Card dialog={quote.dialog} key={key} />;
+const List = ({ data }) => {
+  let quoteCards = data.map((character) => {
+    return <Card name={character.name} key={character._id} />;
   });
 
   return <div className="quotes-container">{quoteCards}</div>;
