16966b846bd1a35d42fdf60bfbdf98173a1e5443
Kat White <kat.white.a@gmail.com>
2021-08-07 11:15:24 -0600

    remove comments

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -43,25 +43,6 @@ export const getCharacterQuotes = (characterId) => {
   }).then((response) => response.json());
 };
 
-// export const getMainCharacters = async () => {
-//   console.log("Character API WAS CALLED");
-
-//   const mainCharacters = await fetch(
-//     "https://the-one-api.dev/v2/character?name=Gandalf,Frodo Baggins,Aragorn II Elessar,Legolas,Arwen,Galadriel,Gimli,Boromir,Bilbo Baggins,Samwise Gamgee,Gollum,Saruman,Peregrin Took,Meriadoc Brandybuck,Elrond,Théoden,Faramir,Éowyn",
-//     {
-//       method: "GET",
-//       headers: {
-//         Accept: "application/json",
-//         Authorization: "Bearer Qdwh7zeW6AgFOOGNGAr4",
-//       },
-//     }
-//   );
-//   const characters = await mainCharacters.json();
-//   const character =
-//     characters.docs[Math.floor(Math.random() * characters.docs.length)];
-//   return character;
-// };
-
 //  const names = [
 //    "Gandalf",
 //    "Frodo Baggins",
