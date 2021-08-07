060593650e81d38a9d7672b47b4ad6dbbab351eb
Kat White <kat.white.a@gmail.com>
2021-08-07 11:15:45 -0600

    remove comments

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,11 +2,11 @@ import React from "react";
 import Card from "../Card/Card";
 import "./List.css";
 
-const List = ({ quotes, speaker }) => {
+const List = ({ quotes, key }) => {
   console.log(quotes, "quotes");
   let quoteCards = quotes.map((quote) => {
     console.log(quote);
-    return <Card dialog={quote.dialog} key={quote._id} id={speaker} />;
+    return <Card dialog={quote.dialog} key={key} />;
   });
 
   return <div className="quotes-container">{quoteCards}</div>;
