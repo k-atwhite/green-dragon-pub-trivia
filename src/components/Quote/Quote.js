aeaeaa6e7d056bd4e76f02b8d3b5fae107829f6f
Kat White <kat.white.a@gmail.com>
2021-08-07 10:44:19 -0600

    Add dependence to useEffect array

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./Quote.css";
 import { getCharacterQuotes } from "../../apiCalls.js";
+import List from "../List/List";
 
 const Quote = ({ characterId }) => {
   const [quotes, setQuotes] = useState([]);
@@ -19,20 +20,20 @@ const Quote = ({ characterId }) => {
         .catch((err) => {
           console.log(err);
         })
-        .then(() =>
-          setRandomQuote(
-            quotes[Math.floor(Math.random() * quotes.length)].dialog
-          )
-        )
         .finally(() => setLoading(false));
     }
-  }, []);
+
+    if (quotes.length) {
+      setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)].dialog);
+    }
+  }, [characterId]);
 
   return (
     <div className="quote-container">
       {loading && <h2>Data is loading</h2>}
-      {/* {quotes.length && <h2>{randomQuote}</h2>} */}
-      <h2>{randomQuote}</h2>
+      {quotes.length && <h2>{randomQuote}</h2>}
+      {/* <h2>{randomQuote}</h2> */}
+      {/* <List quotes={quotes} key={characterId} /> */}
     </div>
   );
 };
