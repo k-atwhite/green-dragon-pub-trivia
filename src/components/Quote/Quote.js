e9676a67723d6fcfe17dd21f48064e7eac689a9c
Kat White <kat.white.a@gmail.com>
2021-08-07 09:06:28 -0600

    Stop infinite re-rendering of useEffect

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import "./Quote.css";
-import { getCharacterQuote } from "../../apiCalls.js";
+import { getCharacterQuotes } from "../../apiCalls.js";
 
 const Quote = ({ characterId }) => {
-  const [quote, setQuote] = useState("");
+  const [quotes, setQuotes] = useState([]);
+  const [randomQuote, setRandomQuote] = useState("");
 
   useEffect(() => {
-    getCharacterQuote(characterId);
-  });
+    if (characterId) {
+      getCharacterQuotes(characterId).then((data) => setQuotes(data.docs));
+    }
+    setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)].dialog);
+  }, []);
 
   return (
     <div className="quote-container">
-      <h2>{quote}</h2>
+      <h2>{randomQuote}</h2>
     </div>
   );
 };
