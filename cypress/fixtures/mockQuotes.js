e36f1fdd6947b1c5c0a4ea424e234286c9433685
Kat White <kat.white.a@gmail.com>
2021-08-08 11:59:20 -0600

    Add mock quote stub to fixtures

---
+++ cypress/fixtures/mockQuotes.js
@@ -0,0 +1,177 @@
+[
+  {
+    name: "State",
+    value: {
+      _id: "5cdbdecb6dc0baeae48cfa59",
+      death: "FO",
+      birth: "TA 2995",
+      hair: "Pale gold",
+      realm: "NaN",
+      height: "Tall",
+      spouse: "Faramir",
+      gender: "Female",
+      name: "Éowyn",
+      race: "Men",
+      wikiUrl: "http://lotr.wikia.com//wiki/%C3%89owyn",
+    },
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1958,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 91,
+    },
+  },
+  {
+    name: "State",
+    value: [
+      {
+        _id: "5cd96e05de30eff6ebccedaa",
+        dialog:
+          "Just to the encampment.  It's tradition for the women of the court to     farewell the men.  ,         The men have found their captain.  They will follow you into battle.  Even     to death.  You have given us hope.  ",
+        movie: "5cd95395de30eff6ebccde5d",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebccedbc",
+        dialog:
+          "My lord, your son......he is dead. , My lord? Uncle? Will you not go to him? Will you do     nothing? ",
+        movie: "5cd95395de30eff6ebccde5b",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebcceddc",
+        dialog: "Your son is badly wounded, my lord.",
+        movie: "5cd95395de30eff6ebccde5b",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebcceddf",
+        dialog: "Theodred.",
+        movie: "5cd95395de30eff6ebccde5b",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebccee50",
+        dialog: "Leave me alone, snake! ",
+        movie: "5cd95395de30eff6ebccde5b",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebccee51",
+        dialog: "Your words are poison.",
+        movie: "5cd95395de30eff6ebccde5b",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebccef3e",
+        dialog: "West' Aragorn, hal ",
+        movie: "5cd95395de30eff6ebccde5d",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebccef4a",
+        dialog: "You are both honourable men.",
+        movie: "5cd95395de30eff6ebccde5d",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebccf0b6",
+        dialog: "What time is it?",
+        movie: "5cd95395de30eff6ebccde5d",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+      {
+        _id: "5cd96e05de30eff6ebccf0ba",
+        dialog:
+          "I dreamed I saw a great wave climbing over green lands and above the     hills. I stood upon the brink. It was utterly dark in the abyss before my     feet. A light shone behind me but I could not turn ,     I could only stand there, waiting. ",
+        movie: "5cd95395de30eff6ebccde5d",
+        character: "5cdbdecb6dc0baeae48cfa59",
+      },
+    ],
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1959,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 85,
+    },
+  },
+  {
+    name: "State",
+    value: "I will kill you if you touch him.",
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1960,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 95,
+    },
+  },
+  {
+    name: "State",
+    value: false,
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1961,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 87,
+    },
+  },
+  {
+    name: "State",
+    value: "",
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1962,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 89,
+    },
+  },
+  {
+    name: "State",
+    value: "",
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1963,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 83,
+    },
+  },
+  {
+    name: "Effect",
+    value: "ƒ () {}",
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1964,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 58,
+    },
+  },
+  {
+    name: "Effect",
+    value: "ƒ () {}",
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1972,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 58,
+    },
+  },
+  {
+    name: "Effect",
+    value: "ƒ () {}",
+    subHooks: [],
+    hookSource: {
+      lineNumber: 1979,
+      functionName: "Quote",
+      fileName: "http://localhost:3000/static/js/main.chunk.js",
+      columnNumber: 58,
+    },
+  },
+];
