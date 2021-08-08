4c59f2b9d9a74182bb05421d566884cd35cfd1a3
Kat White <kat.white.a@gmail.com>
2021-08-08 17:22:01 -0600

    Add error handling

--- cypress/integration/quoteTrivia_spec.js
+++ cypress/integration/quoteTrivia_spec.js
@@ -55,9 +55,18 @@ describe("Trivia Page", () => {
       cy.get(".card").first().click().get("response").should('be.visible')
   })
 
+    it("Should show an error message if quote data cannot be fetched", () => {
+    cy.intercept("https://the-one-api.dev/v2/character?name=Gandalf,Frodo%20Baggins,Aragorn%20II%20Elessar,Legolas,Arwen,Galadriel,Gimli,Boromir,Bilbo%20Baggins,Samwise%20Gamgee,Gollum,Saruman,Peregrin%20Took,Meriadoc%20Brandybuck,Elrond,Théoden,Faramir,Éowyn", { statusCode: 500 });
+    cy.intercept("https://the-one-api.dev/v2/character/5cdbdecb6dc0baeae48cfa59/quote", { statusCode: 500 });
+    cy.visit("http://localhost:3000/trivia");
+    cy.get("error-msg").contains("trouble loading character...");
+  });
+
 //     it("Clicking incorrect character should show sad message"), () => {
 //       cy.get(".card").contains("Éowyn")
 //   }
-// If this is supposed to be random... how do I test for that?
 
 });
+
+// How do I test for randomness
+// How to test for sad path if intercept not working?
