ccd3e0e2044e0476499735f56f5f403d8c1ec628
Kat White <kat.white.a@gmail.com>
2021-08-08 17:22:27 -0600

    Add error handling for hero page

--- cypress/integration/hero_page_spec.js
+++ cypress/integration/hero_page_spec.js
@@ -11,7 +11,7 @@ beforeEach(() => {
   });
 
 
-  cy.visit("http://localhost:3000").get(".geroes").click();
+  cy.visit("http://localhost:3000").get(".heroes").click();
 });
 
 describe("Hero Page", () => {
@@ -21,5 +21,9 @@ describe("Hero Page", () => {
     });
 
      it("Should display an error message if cards cannot be rendered", () => {
+       cy.intercept("https://the-one-api.dev/v2/character?name=Gandalf,Frodo%20Baggins,Aragorn%20II%20Elessar,Legolas,Arwen,Galadriel,Gimli,Boromir,Bilbo%20Baggins,Samwise%20Gamgee,Gollum,Saruman,Peregrin%20Took,Meriadoc%20Brandybuck,Elrond,Théoden,Faramir,Éowyn", { statusCode: 500 });
+      cy.intercept("https://the-one-api.dev/v2/character/5cdbdecb6dc0baeae48cfa59/quote", { statusCode: 500 });
+      cy.visit("http://localhost:3000/heroes");
+      cy.get("error-msg").contains("trouble loading characters...");
     });
 });
