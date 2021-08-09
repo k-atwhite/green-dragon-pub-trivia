23623c93e5a8f44f0cf09f9e676ead0446f4a33d
Kat White <kat.white.a@gmail.com>
2021-08-09 12:32:59 -0600

    Add error message to render function

--- src/components/CharacterQuotes/CharacterQuotes.js
+++ src/components/CharacterQuotes/CharacterQuotes.js
@@ -5,12 +5,17 @@ import "./CharacterQuotes.css";
 
 const CharacterQuotes = ({ hero }) => {
   const [quotes, setQuotes] = useState([]);
+  const [error, setError] = useState([]);
 
   useEffect(() => {
     if (!quotes.length) {
       getCharacterQuotes(hero._id).then((data) => setQuotes(data.docs));
     }
   }, [quotes]);
+
+  if (!hero) {
+    setError("We are having trouble loading quotes")
+  }
 
   if (quotes.length) {
     let quoteCards = quotes.map((quote) => {
@@ -19,6 +24,7 @@ const CharacterQuotes = ({ hero }) => {
 
     return (
       <div>
+        {error && <h2 classNmae="error-msg">{error}</h2>}
         <h2>Words of: {hero.name}</h2>
         <div className="quote-cards-container">{quoteCards}</div>;
       </div>
