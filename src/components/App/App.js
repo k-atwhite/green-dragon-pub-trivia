ba57b456e09fad3dde3cb9c4628873887139b34c
Kat <kat.white.a@gmail.com>
2021-09-26 08:47:50 -0600

    Add media queries for laptop size

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getAllCharacters } from '../../apiCalls.js';
-import { Route, Switch, Link, Redirect } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import Quote from '../Quote/Quote';
 import Characters from '../Characters/Characters';
@@ -65,7 +65,6 @@ const App = () => {
           }}
         />
       </Switch>
-      {/* <Redirect to={'/'} /> */}
     </main>
   );
 };
